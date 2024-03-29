6cd4109c33d1ac37ff3850e61258d61025b90da4
Eelismey-jf <eelismey@gmail.com>
2024-03-29 17:17:57 +0100

    starting js conversion

--- routes.js
+++ routes.js
@@ -1,9 +1,10 @@
 // Core:
+import { unlink, createReadStream } from "fs";
+import { pipeline } from "stream/promises";
 // Local:
 import { jsUpload, jsCheck, Js2Json } from "./Functions/js-2-json.js";
 // 3rd Party:
 import Express from "express";
-
 // destruct/constants/variables
 const convertPath = Express.Router();
 
@@ -11,7 +12,7 @@ convertPath.post("/js-2-json", jsUpload, async(req, res, next )=>{
 
         console.log(req.body, req.file); 
         const checker = jsCheck(req.file); 
-        console.log(checker);
+        console.log("checker :" + checker);
 
         switch (checker.code.toString()[0]) {
           case "4": 
@@ -24,9 +25,28 @@ convertPath.post("/js-2-json", jsUpload, async(req, res, next )=>{
           break; 
           default:
           const newFile = await Js2Json();
-          console.log("new file: " + newFile.file.toString());
-          res.download(newFile.file.toString());
-          res.status(Js2Json.code || 201).send(Js2Json.msg);
+// can use "res.download(newFile.file.toString());" but nothing can be done after it, contrary to res.writeHead.
+          const { newFileName, originalFilePath, filePath} = newFile;
+          const options = {
+            "Content-Type": "application/json",
+            "Content-Source": "upload",
+            "Document-Name": newFileName,
+            "Transmission": "download"
+          };
+          res.writeHead(201, { options });
+          await pipeline(createReadStream(filePath), res);
+          await Promise.all(
+            [originalFilePath, filePath].map((file, _ind)=>{ 
+              unlink(file, (err)=>{
+                if(err){
+                  console.error("Cannot destruct file nº " + Number(_ind+1) + ": ", err)
+                } else {
+                  console.log("File nº " + Number(_ind+1) + " destruction OK")
+                }
+              })
+            })
+            );
+            return true;
         }
       })
 
