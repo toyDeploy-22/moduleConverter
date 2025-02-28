d09e730aa5f6d393c30cbbc6561c38a47e126525
Eelismey-jf <eelismey@gmail.com>
2025-02-28 22:11:24 +0100

    rectifier

--- Functions/pdf-2-csv.js
+++ Functions/pdf-2-csv.js
@@ -1,7 +1,7 @@
 // Core:
 import  { dirname, join, extname } from "path";
 import { fileURLToPath } from "url";
-import fs from "fs";
+import { readFile, createWriteStream } from "fs";
 // Local:
 // 3rd Party:
 import multer from "multer";
@@ -9,14 +9,10 @@ import PDFParser from "pdf2json";
 // https://www.npmjs.com/package/pdfkit
 // https://github.com/modesty/pdf2json/npm
 
-// destruct/constants/variables
-const { createWriteStream } = fs;
-
 const conversionFolder = join(dirname(fileURLToPath(import.meta.url)), "..", "Conversion");
 
 const d = new Date;
 let newPdfFile;
-const pdfParser = new PDFParser();
 
 const pdfStorage = multer.diskStorage({
     destination: join(conversionFolder, "UPLOAD"),
@@ -38,6 +34,7 @@ const pdfUpload = multer({
         });
     
     multer({ storage: folderCheck});
+
     
 function pdfCheck(file) {
     const extensionFormat = extname(file.originalname).toLowerCase(); // returns extension preceded by a dot "."
@@ -64,39 +61,11 @@ function pdfCheck(file) {
 const newCsvFile = "New_Convert-" + d.getTime() + ".csv"; 
 const csvPath = join(conversionFolder,"CSV");
 
-const extractContent = (pdfFile) => {
-
-    const pdfFilePath =  join(join(conversionFolder,"UPLOAD"), pdfFile);
-    let result = new Object();
-    
-    pdfParser.on("pdfParser_dataReady", (pdfBuffer) => {
-    result = {
-        error: false,
-        data: JSON.stringify(pdfBuffer)
-    }
-    
-    return result
- })
-
-  pdfParser.on("pdfParser_dataError", (err)=>{
-    console.error(err);
-    result = {
-        error: true,
-        msg: 'Could not access to PDF content.',
-        ...err
-        }
-    return result
-    })
-
-    pdfParser.loadPDF(pdfFilePath)
-
- }
-
 const contentToCsv = (content) => {
     try {
-    const csvContent = content.replace(/[&\/\\#,+()$~%.'":*?<>{}]/g, '').split(" ");
-    
-    return csvContent.join(",");
+    const csvContent = content.replace(/[&\/\\#,+()$~%.'":*?<>{}]/g, '');
+    return csvContent.split(" ");
+    // return csvContent.join(",");
     } catch(err) {
         const errMsg = `: ${err.message}` || ". Check object above";
         console.error(err);
@@ -104,66 +73,95 @@ const contentToCsv = (content) => {
     }
 }
 
-const csvMaking = async(newFileName) => {
+	// Promise Logic
+	
+	/**
+	const pdfPromise = (filename) => {
+	const pdfParser = new PDFParser();
+	return new Promise((resolve, reject) => {
+		
+		pdfParser.on("pdfParser_dataError", (err) => {
+			reject({error: true, msg: "An error occured during parsing." || err.message})
+		});
+		
+		pdfParser.on("pdfParser_dataReady", (pdfData) => {
+			resolve({
+			error: false,
+			data: pdfData.Pages[0].Texts
+		})});
 
-try {
+	 pdfParser.loadPDF(join(conversionFolder, filename))
+	})
+	}
+	
+	const promiseToCSV = (filename, csvFile) => {
+		pdfPromise(filename)
+		.then((pdfData) => {
+			const newBuffer = pdfData.data;
+			const bufferWriteStream = createWriteStream(join(csvPath, csvFile));
+			newBuffer.forEach((txt) => {
+			let strPDF = txt.R[0].T;
+			let pdfSpaces = strPDF.replaceAll(/[%20%%22%]/g, " ");
+			let pdf2CSV = contentToCsv(pdfSpaces);
+			bufferWriteStream.write(pdf2CSV)})})
+		.finally(() => console.log("success"))
+		.catch((err) => console.error(err))
+	}
+
+NOT NEEDED
+
+const csvMaking = async(csvFolder, newcsv) => {
+
     const result = new Object();
          //Omit option to extract all text from the pdf file
         //Omit option to extract all text from the pdf file
-    const pdfData = extractContent(newPdfFile);
+	try{
+	const csvPathFile = join(join(conversionFolder,"CSV"), newcsv);
+	result.error = false;
+	return result
+	} catch(err){
+	console.error(err);
+	result.error = true;
+	result.msg = err.message || "Error at CSV edition. Check above fore more results";
+	return result
+	}
+  }   
+**/
 
-    if(pdfData.error) {
-        result.error = true;
-        result.msg = pdfData.msg;
-        return result
-    } else {
-        const pdfBuffer = await pdfData.data;
-        const PDFToCSV = await contentToCsv(pdfBuffer);
-        
-        const csvContent = createWriteStream(join(csvPath, newFileName));
-        csvContent.write(PDFToCSV);
-
-        result.error = false;
-        result.msg = "success";
-        return result        
-        }
-    } catch(err) {
-    const errMsg = `: ${err.message}` || ". Check object above";
-    console.error(err);
-    console.log("An error occured during writing conversion: ", errMsg)
-    result.error = true;
-    result.msg = errMsg;
-    return result
-    }        
-}
-
-async function Pdf2Csv() { 
-    
+async function Pdf2Csv(data) { 
     let result = {};
 
     try{
-       
-    const createCSV = await csvMaking(newCsvFile);
-    // await writeFile(join(join(conversionFolder, "./JSON"), newJsonName), jsonBuffer); 
-    if(createCSV.error) {
-    result = {
-    error: true,
-    code: 401,
-    uploadFolder: join(conversionFolder,"UPLOAD"),
-    msg: "Something went wrong during the CSV creation: " + createCSV.msg
-        }
-    return result
-    } else {
+	let i = 0;
+	const bufferWriteStream = createWriteStream(join(csvPath, newCsvFile));
+	
+	for(i; i < data.length; i++) {
+			let strPDF = data[i].R[0].T;
+			let splitter = strPDF.split(/[%20%%22%]/g);
+			let noSpace = splitter.filter((wrd) => wrd !== "");
+			let pdfSpaces = noSpace.join(" ");
+			let pdf2CSV = contentToCsv(pdfSpaces);
+			data.length - 1 === i ? bufferWriteStream.write(`"${pdf2CSV.join(" ")}"`) : bufferWriteStream.write(`"${pdf2CSV.join(" ")}", `)
+	}
+	
+	/*
+	data.forEach((txt) => {
+			let strPDF = txt.R[0].T;
+			let pdfSpaces = strPDF.replaceAll(/[%20%%22%]/g, " ");
+			let pdf2CSV = contentToCsv(pdfSpaces);
+			bufferWriteStream.write(pdf2CSV)})
+	*/
+
     result = {
     error: false,
     code: 201,
     newFileName: newCsvFile,
-    uploadFolder: join(conversionFolder,"UPLOAD"),
+    uploadFolder: join(conversionFolder, "UPLOAD"),
     originalFilePath: join(join(conversionFolder,"UPLOAD"), newPdfFile),
-    filePath: join(join(conversionFolder, "CSV"), newCsvFile),
-    msg: "PDF file successfully converted to CSV. Ready for download."}}
-    return result
-    } catch(err) { 
+    filePath: join(csvPath, newCsvFile),
+    msg: "PDF file successfully converted to CSV. Ready for download."}
+    return result 
+	} catch(err) { 
         const errMsg = `: ${err.message}` || ". Check object above";
         console.error(err);
         result = {
--- pdf-csv-routes.js
+++ pdf-csv-routes.js
@@ -1,4 +1,6 @@
 // Core:
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
 // import { unlink } from "fs";
 import { emptyDir, remove } from "fs-extra";
 // import { pipeline } from "stream/promises";
@@ -7,9 +9,53 @@ import { pdfUpload, pdfCheck, Pdf2Csv } from "./Functions/pdf-2-csv.js";
 // 3rd Party:
 import cors from "cors";
 import Express from "express";
+import PDFParser from "pdf2json";
+
 // destruct/constants/variables
 const pdf_csv_conv = Express.Router();
 let downloadFile = {};
+const conversionFolder = join(dirname(fileURLToPath(import.meta.url)), "Conversion", "UPLOAD");
+const pdfParser = new PDFParser();
+
+// functions
+const pdfPromise = (filename) => {
+
+	return new Promise((resolve, reject) => {
+		
+		pdfParser.on("pdfParser_dataError", (err) => {
+			reject({
+				error: true,
+				msg: err.message || "Parsing PDF Data Rejection"
+			})
+		});
+		
+		pdfParser.on("pdfParser_dataReady", (pdfData) => {
+			resolve({
+				error: false,
+				data: pdfData.Pages[0].Texts
+				})
+		});
+
+	 pdfParser.loadPDF(join(conversionFolder, filename))
+		})
+	}
+	
+	const promiseToCSV = async(filename) => {
+		const promiseResult = {};
+		try {
+		const promiseData = await pdfPromise(filename);
+		const filePromise = await promiseData.data;
+			promiseResult.err = false;
+			promiseResult.data = filePromise;
+			// console.log("finally success: ", promiseResult)
+		} catch(err) {
+			promiseResult.err = true;
+			promiseResult.msg = `extraction Data error: ${err.message}` || "An error occured during data extraction."
+		}
+		
+		return promiseResult
+	}
+
 
 		pdf_csv_conv.options("/pdf-2-csv", cors())
         pdf_csv_conv.post("/pdf-2-csv", cors(), pdfUpload, async(req, res, next )=>{  
@@ -32,8 +78,10 @@ let downloadFile = {};
               });
               res.status(checker.code).send(checker.msg); 
               break; 
-              case "2":   
-              const newCsvFile = await Pdf2Csv();
+              case "2":
+			  const fileToCSV = await promiseToCSV(req.file.filename);
+              const newCsvFile = await Pdf2Csv(fileToCSV.data);
+			  // console.log('FNAME: ', fileToCSV)
     // can use "res.download(newFile.file.toString());" but nothing can be done after it, contrary to res.writeHead.
              if(!newCsvFile.error) {
              downloadFile = { ...newCsvFile };
